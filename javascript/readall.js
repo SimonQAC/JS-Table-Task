29ceef96101033a7175a4245f023ae9e06a12871
Simon Peppitt <simonworkpersonal@gmail.com>
2020-09-25 13:13:45 +0100

    update homepage, fixed read

--- javascript/readall.js
+++ javascript/readall.js
@@ -7,5 +7,55 @@ fetch('http://jsonplaceholder.typicode.com/posts')
                 console.log('Error ' + response.status);
             return;
             }
+            //captures data from fetch, and outputs it
+            //in the console
+            response.json().then(function(data1){
+                console.log("data: ",data1);
+                let table = document.querySelector("table");
+                let data = Object.keys(data1[0]);
+                
+                //calls the generate head 
+                //and generate table functions
+                generateTableHead(table,data);
+                generateTable(table,data1);
+            });
         }
     )
+    //finds and prints any fetch errors to console
+    .catch(function(error){
+        console.log('Fetch Error: ',error)
+    }
+    );
+
+    function generateTableHead(table, data) {
+        let thead = table.createTHead();
+        let row = thead.insertRow();
+        for (let key of data) {
+          let th = document.createElement("th");
+          let text = document.createTextNode(key);
+          
+          th.appendChild(text);
+          row.appendChild(th);
+        }
+      }
+      
+      function generateTable(table, data) {
+        for (let element of data) {
+          let row = table.insertRow();
+          for (key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+          }
+          let cell1 = row.insertCell();
+          let text1 = document.createElement("a");
+        }
+      }
+      
+      const params = new URLSearchParams(window.location.search);
+      
+      console.log(params)
+      
+      for(const param of params){
+        console.log(param);
+      }
